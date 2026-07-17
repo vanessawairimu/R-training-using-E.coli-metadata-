456e4f6005a98875d93f3cad37a9b19eb2d2b988
vanessawairimu <vanessawairimu29@gmail.com>
2026-07-17 08:23:03 +0300

    Add files via upload

---
+++ E.coli metadata.R
@@ -0,0 +1,16 @@
+mass <- 47.5
+age <- 122
+mass <- mass*2.3
+age <- age-20
+mass <- 47.5
+age <- 122
+mass <- mass*2.3
+age <- age - 20
+mass / age
+mass > age
+age > mass
+rm
+rm(age, mass)
+installed.packages("ggplot2")
+install.packages("gapminder")
+install.packages("ggplot2")
